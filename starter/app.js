ed5a4cad420b90ddda797559c0e7c58f702090b5
wanfadger <galiwango.fahad@gmail.com>
2021-11-15 20:15:56 +0300

    added config for variables
    swicthed btn prod and dev environents

--- starter/app.js
+++ starter/app.js
@@ -1,57 +1,31 @@
 const express = require('express');
-const morgan = require("morgan")
+const morgan = require('morgan');
+const dotenv = require('dotenv');
 // const { json } = require('express');
+
+//setting env variables
+dotenv.config({
+  path: `${__dirname}/config.env`,
+});
 
 const tourRouter = require(`./routes/tour-router`);
 const userRouter = require(`./routes/user-router`);
 
 const app = express();
 
-
-
+// console.log(process.env);
+console.log(app.get('env'));
+console.log(process.env.USERNAME);
 //////////////////MIDDLE WARES
 
 //overview.html middleware
 
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
+if(process.env.NODE_ENV === "development")  app.use(morgan('dev'));
 
 app.use(express.json());
-app.use(morgan("dev"))
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// app.use((req, res, next) => {
-//     console.log("Hello From The middleware")
-//     next()
-// })
-
-// app.use((req, res, next) => {
-//    req.company = "Planet Systems Limited"
-//   next();
-// });
-
-
-
-
-/////////////ROUTE HANDLERS
-
-
-
-// //get
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// /////////////////////////[post]
-// app.post('/api/v1/tours', createTour);
-// ///////////patch
-// app.patch('/api/v1/tours/:id', pathTour);
-// ///////////delete
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-
-
-
-module.exports = app
-
-
-
+module.exports = app;
--- starter/server.js
+++ starter/server.js
@@ -1,7 +1,7 @@
 
 const app = require("./app")
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}`);
 });
