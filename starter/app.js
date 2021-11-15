dd9a3d056dca0fa921eb4b9a2be3dd85adece5bf
wanfadger <galiwango.fahad@gmail.com>
2021-11-15 12:39:15 +0300

    implemented post request
    saved data into json file

--- starter/app.js
+++ starter/app.js
@@ -1,29 +1,48 @@
-const fs = require("fs")
-const express = require("express")
+const fs = require('fs');
+const express = require('express');
+const { json } = require('express');
 
-const app = express()
+const app = express();
 
+//middlewares
+app.use(express.json());
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, "utf-8"))
-app.get("/api/v1/tours", (req, res) => {
-    res
-        .status(200)
-        .json({
-            message: "success",
-            count: tours.length,
-            data: { tours },
-            status:true
-        })
-        //.send("Hello World from server side")
-} )
-
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf-8')
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    message: 'success',
+    count: tours.length,
+    data: { tours },
+    status: true,
+  });
+  //.send("Hello World from server side")
+});
 
 /////////////////////////[post]
-app.post("/", (req, res) => {
-    res.send("posting data")
-})
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  const newId = tours[tours.length - 1] + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
 
-let port = 3000
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours.json`,
+    JSON.stringify(tours),
+    (error, data) => {
+      if (error) console.log('Error ' + error.message);
+      res.status(201).json({
+        message: 'success',
+        data: { trour: newTour },
+        status: true,
+      });
+    }
+  );
+});
+
+let port = 3000;
 app.listen(port, () => {
-    console.log(`app running on port ${port}`)
-})
+  console.log(`app running on port ${port}`);
+});
