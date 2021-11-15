a0df5e8856f2d3a97b018dc3129e739676e3b462
wanfadger <galiwango.fahad@gmail.com>
2021-11-15 12:10:48 +0300

    implemented a get request
    first read data syshchronously on server load
    
    created json response and returned it

--- starter/app.js
+++ starter/app.js
@@ -1,15 +1,27 @@
+const fs = require("fs")
 const express = require("express")
 
 const app = express()
 
 
-app.get("/", (req, res) => {
-    console.log(req)
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, "utf-8"))
+app.get("/api/v1/tours", (req, res) => {
     res
         .status(200)
-        .json({ message: "Hello World from server side" })
+        .json({
+            message: "success",
+            count: tours.length,
+            data: { tours },
+            status:true
+        })
         //.send("Hello World from server side")
 } )
+
+
+/////////////////////////[post]
+app.post("/", (req, res) => {
+    res.send("posting data")
+})
 
 let port = 3000
 app.listen(port, () => {
