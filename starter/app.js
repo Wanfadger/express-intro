95d4bb3198bea51640bff7ce0b6fecf27ec7f4fc
wanfadger <galiwango.fahad@gmail.com>
2021-11-17 08:16:03 +0300

    created mondodb connection using mongoose

--- starter/app.js
+++ starter/app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose')
 // const { json } = require('express');
 
 //setting env variables
 dotenv.config({
   path: `${__dirname}/config.env`,
 });
+
+//DATABASE CONNECTION
+mongoose
+  .connect(process.env.DATABASE_URL_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    // console.log(conn);
+    console.log('Db Connection Successful');
+  });
+
 
 const tourRouter = require(`./routes/tour-router`);
 const userRouter = require(`./routes/user-router`);
