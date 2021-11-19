c72077cd7a5b3233018bc70f476d72e99867328f
wanfadger <galiwango.fahad@gmail.com>
2021-11-19 19:00:24 +0300

    added helmet to manage headers

--- starter/app.js
+++ starter/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
-const AppError = require(`${__dirname}/utils/globalError`)
+const AppError = require(`${__dirname}/utils/globalError`);
 
 // const { json } = require('express');
 
@@ -24,21 +25,32 @@ console.log(process.env.USERNAME);
 
 //overview.html middleware
 
-app.use(express.static(`${__dirname}/public`));
+// SECURITY HTTP HEADER
+app.use(helmet());
 
+// DEVELOPMENT LOGGING
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 const limiter = rateLimit({
   max: 100,
   windowMs: 1000 * 60 * 60,
-  message:"Too many request from this Ip, try again in an hour"
-})
+  message: 'Too many request from this Ip, try again in an hour',
+});
 
+/// LIMIT REQUEST FROM SAME API
+app.use('/api', limiter);
 
-app.use("/api" , limiter)
+// BODY PARSER , READING DATA FROM BODY INTO REQ.BODY
 app.use(express.json());
+
+// TO TOUR ROUTES
 app.use('/api/v1/tours', tourRouter);
+
+// TO USER ROUTERS
 app.use('/api/v1/users', userRouter);
+
+// SERVING STATIC FILES
+app.use(express.static(`${__dirname}/public`));
 
 //unmatched routes middleware
 app.all('*', (req, res, next) => {
@@ -47,12 +59,12 @@ app.all('*', (req, res, next) => {
 
 //global error handling middleware , fired in catch blocks
 app.use((error, req, res, next) => {
-  error.statusCode = error.statusCode || 500
-    res.status(error.statusCode).json({
-      message: error.message,
-      status: false,
-      statusCode: error.statusCode,
-    });
-})
+  error.statusCode = error.statusCode || 500;
+  res.status(error.statusCode).json({
+    message: error.message,
+    status: false,
+    statusCode: error.statusCode,
+  });
+});
 
 module.exports = app;
